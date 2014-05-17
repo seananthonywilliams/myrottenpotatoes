4bb23da698b51efa76be616a50926d77ce0b8550
Sean Williams <seananthonywilliams@gmail.com>
2014-05-17 01:37:56 -0300

    first commit of rails new autogenerated files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = 'ac0c0ace0ffb28f5446464cd31912f2b8b625ae740986a78a8442f191b5b540a67a2efdeab8f59f13121cf3740c738ca4d4280ea6715f9414b71ce0236affb0a'
