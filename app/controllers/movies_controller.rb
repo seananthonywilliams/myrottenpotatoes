d1675e0870099422084d012a210a60170c2e4814
Sean Williams <seananthonywilliams@gmail.com>
2014-05-17 16:20:10 -0300

    Added views, movies controller, and a CSS

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,9 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+  def show
+    @movie = Movie.find_by_id(params[:id]) # what if this movie is not in DB? 
+    # will render app/views/movies/show.html.haml by default
+  end
+end
