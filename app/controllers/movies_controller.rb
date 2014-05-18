6b484fcdb9008871a9d56691b01864217ae48917
Sean Williams <seananthonywilliams@gmail.com>
2014-05-17 22:30:58 -0300

    form submission: new and create

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,7 @@ class MoviesController < ApplicationController
     @movie = Movie.find_by_id(params[:id]) # what if this movie is not in DB? 
     # will render app/views/movies/show.html.haml by default
   end
+  def new
+    # default: render 'new' template
+  end
 end
