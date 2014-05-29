efaa26343c54702fd7935203e65a0ac97b7ca88d
Sean Williams <seananthonywilliams@gmail.com>
2014-05-29 01:07:36 -0300

    7.8 Enhancing RottenPotatoes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Myrottenpotatoes::Application.routes.draw do
+  
+  # Route that posts 'Search TMDb' form
+  post '/movies/search_tmdb'
+
   resources :movies
   root :to => redirect('/movies')
 end
